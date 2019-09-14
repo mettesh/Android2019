3397a4ab342ef74cb474b21a03e0dd4410fdc8dd
Mette <mette-strand@live.no>
2019-09-14 14:28:07 +0200

    Added animation to header texts

--- app/src/main/java/no/hiof/mettesh/utdanningsoversikten/MainActivity.kt
+++ app/src/main/java/no/hiof/mettesh/utdanningsoversikten/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +15,8 @@ class MainActivity : AppCompatActivity() {
         //Vise startLayout
         setContentView(R.layout.activity_splash_screen)
 
+        animateHeader()
+
         Handler().postDelayed({
             startActivity(Intent(this@MainActivity, FirstActivity::class.java))
             finish()
@@ -20,6 +24,16 @@ class MainActivity : AppCompatActivity() {
 
 
         //Laste inn data fra API?
+    }
+
+    fun animateHeader(){
+        val animationUtdanning = AnimationUtils.loadAnimation(this, R.anim.anim_utdanning)
+        start_layout_text1.startAnimation(animationUtdanning)
+
+        val animationOversikten = AnimationUtils.loadAnimation(this, R.anim.anim_oversikten)
+        start_layout_text2.startAnimation(animationOversikten)
+
+
     }
 
 }
