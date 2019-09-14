547924fedc2e8b34eeca991c95e58a22faf90a5b
Mette <mette-strand@live.no>
2019-09-14 14:02:35 +0200

    Implemented splash screen

--- app/src/main/java/no/hiof/mettesh/utdanningsoversikten/MainActivity.kt
+++ app/src/main/java/no/hiof/mettesh/utdanningsoversikten/MainActivity.kt
@@ -1,12 +1,25 @@
 package no.hiof.mettesh.utdanningsoversikten
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //Vise startLayout
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this@MainActivity, FirstActivity::class.java))
+            finish()
+        }, 4000)
+
+
+        //Laste inn data fra API?
     }
+
 }
