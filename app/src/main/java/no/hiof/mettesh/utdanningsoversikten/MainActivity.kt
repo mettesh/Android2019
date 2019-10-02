886cf1a6995dd102b95136032b62f2d241abd5c2
Mette <mette-strand@live.no>
2019-10-02 08:24:25 +0200

    Changed filenames

--- app/src/main/java/no/hiof/mettesh/utdanningsoversikten/MainActivity.kt
+++ app/src/main/java/no/hiof/mettesh/utdanningsoversikten/MainActivity.kt
@@ -1,39 +1,12 @@
 package no.hiof.mettesh.utdanningsoversikten
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.view.animation.AnimationUtils
-import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        //Vise startLayout
-        setContentView(R.layout.activity_splash_screen)
-
-        animateHeader()
-
-        Handler().postDelayed({
-            startActivity(Intent(this@MainActivity, FirstActivity::class.java))
-            finish()
-        }, 4000)
-
-
-        //Laste inn data fra API?
+        setContentView(R.layout.activity_main)
     }
-
-    fun animateHeader(){
-        val animationUtdanning = AnimationUtils.loadAnimation(this, R.anim.anim_utdanning)
-        start_layout_text1.startAnimation(animationUtdanning)
-
-        val animationOversikten = AnimationUtils.loadAnimation(this, R.anim.anim_oversikten)
-        start_layout_text2.startAnimation(animationOversikten)
-
-
-    }
-
 }
