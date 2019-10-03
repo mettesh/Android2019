a39db07a7c1cdf392b80a8900c34ab0a3ae7ae81
Marte Olavsdatter Ørvild <marte.olavsdatter@gmail.com>
2019-10-03 12:18:53 +0200

    Added bottom navigation to main activity

--- app/src/main/java/no/hiof/mettesh/utdanningsoversikten/MainActivity.kt
+++ app/src/main/java/no/hiof/mettesh/utdanningsoversikten/MainActivity.kt
@@ -2,11 +2,27 @@ package no.hiof.mettesh.utdanningsoversikten
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Referanse til navController
+        var navController = findNavController(R.id.listFragment)
+
+        // Setter opp bottom navigation
+        setupBottomNavMenu(navController)
+    }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        bottomNav?.setupWithNavController(navController)
     }
 }
