4ae79b7117f0bbf623a8cf7b7e4a9a7746dbdefd
Mette <mette-strand@live.no>
2019-10-06 21:26:26 +0200

    small changes to education list item-xml

--- app/src/main/java/no/hiof/mettesh/utdanningsoversikten/adapter/EducationAdapter.kt
+++ app/src/main/java/no/hiof/mettesh/utdanningsoversikten/adapter/EducationAdapter.kt
@@ -1,11 +1,15 @@
 package no.hiof.mettesh.utdanningsoversikten.adapter
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.education_list_item.view.*
@@ -80,7 +84,7 @@ class EducationAdapter(private val items: ArrayList<Education>, var clickListene
             educationShortDescriptionTextView.text = item.descriptionShort
 
             schoolWebLink.setOnClickListener {
-                
+
             }
 
             // Sets the onClickListener
