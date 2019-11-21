b7c4402696b551e9da7cde7970808271ca463994
Marte Olavsdatter <marte.olavsdatter@gmail.com>
2019-11-21 14:02:43 +0100

    Added logic for individual marker icons

--- app/src/main/java/no/hiof/mettesh/utdanningsoversikten/MarkerClusterItem.kt
+++ app/src/main/java/no/hiof/mettesh/utdanningsoversikten/MarkerClusterItem.kt
@@ -1,11 +1,12 @@
 package no.hiof.mettesh.utdanningsoversikten
 
+import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.clustering.ClusterItem
 
 
 
-class MarkerClusterItem(val latLng: LatLng, val markerTitle: String = "", val snippetText: String = ""): ClusterItem{
+class MarkerClusterItem(val latLng: LatLng, val markerTitle: String = "", val snippetText: String = "", val icons: BitmapDescriptor): ClusterItem{
     override fun getSnippet(): String {
         return snippetText
     }
@@ -16,5 +17,9 @@ class MarkerClusterItem(val latLng: LatLng, val markerTitle: String = "", val sn
 
     override fun getPosition(): LatLng {
         return latLng
+    }
+
+     fun getICon(): BitmapDescriptor {
+        return icons
     }
 }
